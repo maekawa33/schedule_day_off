535d7da22cdcf25f3e64f09c75c836b1ab6cb8ca
macmini <u19b138k@wako.ac.jp>
2023-01-19 16:49:40 +0900

    register_mailにnameを追加 #12

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -2,13 +2,13 @@ class UserSessionsController < ApplicationController
   skip_before_action :require_login
   
   def new; end
-  
+
   def login_mail; end
 
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_back_or_to root_path
+      redirect_back_or_to new_user_path
     else
       render :new
     end
