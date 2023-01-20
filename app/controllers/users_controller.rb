d33d295af4ee69ee8d172b513f20b4fa113f3370
macmini <u19b138k@wako.ac.jp>
2023-01-20 13:41:54 +0900

    fix: Rubocopを使用#18

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,12 +14,12 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def edit;end
+  def edit; end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to login_path, success: "User was successfully created." 
+      redirect_to login_path, success: 'User was successfully created.'
     else
       render :new
     end
@@ -27,23 +27,24 @@ class UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      redirect_to user_url(@user), notice: "User was successfully updated." 
+      redirect_to user_url(@user)
     else
-      render :edit, status: :unprocessable_entity 
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
     @user.destroy
-    redirect_to users_url, notice: "User was successfully destroyed." 
+    redirect_to users_url
   end
 
   private
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
