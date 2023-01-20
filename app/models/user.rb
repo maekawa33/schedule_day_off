debd759cfcd9fe84b89e0d8946b44a12b928bd97
macmini <u19b138k@wako.ac.jp>
2023-01-20 15:35:08 +0900

    fix: rubocop #20

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   has_many :favorites
   has_many :schedules
-  
+
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
