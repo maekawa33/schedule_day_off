9b93f54eeca0f1de628192b59ae0070c50187ed4
macmini <u19b138k@wako.ac.jp>
2023-01-20 11:45:01 +0900

    add: static_pagesの作成 #16

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'top', to: 'static_pages#top'
+  get 'privacy_policy', to: 'static_pages#privacy_policy'
+  get 'terms_of_service', to: 'static_pages#terms_of_service'
   get 'login', to: 'user_sessions#new'
   get 'login_mail', to: 'user_sessions#login_mail'
   post 'login', to: 'user_sessions#create'
   get 'logout', to: 'user_sessions#destroy'
 
+  root to: 'static_pages#top'
   resources :users, only: %i[new create show] do
     collection do
       get :register_mail
