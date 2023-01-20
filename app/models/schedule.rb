4098f0cc46373dace865ba6994108c23a56d23b5
macmini <u19b138k@wako.ac.jp>
2023-01-20 14:49:04 +0900

    add: scheduleがmain_eventを三つ持つバリデーションの追加 #20

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -3,4 +3,17 @@ class Schedule < ApplicationRecord
 
   validates :schedule_title, presence: true
   validates :assumed_number_people, presence: true
+  validate :three_main_events_validate
+  def three_main_events_validate
+    three_main_events = 3
+    main_event_count = 0
+    events.each do |a|
+      if a.three_main_events == true
+        main_event_count += 1
+      end
+    end
+    if main_event_count != three_main_events
+      errors.add(:events, "main_events fail")
+    end
+  end  
 end
