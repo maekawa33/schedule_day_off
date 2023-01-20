b1cb24fb26d9c1b23b1098fa691ae32ccbebbe0f
macmini <u19b138k@wako.ac.jp>
2023-01-20 23:20:53 +0900

    add: 画像アップロード機能の追加 #22

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :schedule
 
+  mount_uploader :image, ImageUploader
+
   validates :start_time, presence: true
   validates :end_time, presence: true
   validates :event_title, presence: true
