2ad4556ca150e4aa3fb868cfde3e551c6a7c91e4
macmini <u19b138k@wako.ac.jp>
2023-01-26 20:57:50 +0900

    add: 起床時間、就寝時間カラムの追加#32

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_21_143820) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_26_115230) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_21_143820) do
     t.integer "assumed_number_people"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.time "get_up_time", null: false
+    t.time "sleep_time", null: false
     t.index ["user_id"], name: "index_schedules_on_user_id"
   end
 
