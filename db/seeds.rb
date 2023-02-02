e3a4d346fa46e5d9037f1558b273600fd361f233
Maekawa Kouki <u19b138@wako.ac.jp>
2023-02-02 12:50:14 +0900

    add: seedファイルの作成#38

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,56 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+5.times {
+  User.create!(
+    name: Faker::Name.unique.name,
+    email: Faker::Internet.unique.email,
+		role: 0,
+		password: 'password',
+		password_confirmation: 'password',
+		avatar: open("./app/assets/images/default_icon.png")
+  )
+}
+
+User.all.each do |user|
+  Schedule.create!(
+    schedule_title: Faker::Hobby.activity,
+    assumed_number_people: Faker::Number.between(from:1, to:5),
+		get_up_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+		sleep_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+    user: user,
+
+    events_attributes: [
+      {
+          start_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+          end_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+          event_title: Faker::Hobby.activity,
+          image: open("./app/assets/images/default_icon.png"),
+          price: Faker::Number.between(from:1, to:10000),
+          store: Faker::Internet.url,
+          comment: Faker::Hobby.activity,
+          three_main_events: true
+      },
+      {
+        start_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+        end_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+        event_title: Faker::Hobby.activity,
+        image: open("./app/assets/images/default_icon.png"),
+        price: Faker::Number.between(from:1, to:10000),
+        store: Faker::Internet.url,
+        comment: Faker::Hobby.activity,
+        three_main_events: true
+    },
+    {
+      start_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+      end_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+      event_title: Faker::Hobby.activity,
+      image: open("./app/assets/images/default_icon.png"),
+      price: Faker::Number.between(from:1, to:10000),
+      store: Faker::Internet.url,
+      comment: Faker::Hobby.activity,
+      three_main_events: true
+  }
+    ]
+    
+  
+  )
+end
+
