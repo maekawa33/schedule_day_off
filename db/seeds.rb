0e00456b45f864020de53f67feb5d38178545319
Maekawa Kouki <maekawa.kouki33@gmail.com>
2023-02-24 15:05:52 +0900

    fix: 挿入データをフォームに合わせたものに修正#59

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,39 +17,22 @@ User.all.each do |user|
       get_up_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
       sleep_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
       user: user,
+    )
+  }
+end
   
-      events_attributes: [
-        {
-            start_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
-            end_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
-            event_title: Faker::Alphanumeric.alpha(number: 10),
-            image: open("./app/assets/images/default_icon.png"),
-            price: Faker::Number.between(from:1, to:10000),
-            store: Faker::Internet.url,
-            comment: Faker::Hobby.activity,
-            three_main_events: true
-        },
-        {
-          start_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
-          end_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
-          event_title: Faker::Alphanumeric.alpha(number: 10),
-          image: open("./app/assets/images/default_icon.png"),
-          price: Faker::Number.between(from:1, to:10000),
-          store: Faker::Internet.url,
-          comment: Faker::Hobby.activity,
-          three_main_events: true
-      },
-      {
-        start_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
-        end_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
-        event_title: Faker::Alphanumeric.alpha(number: 10),
-        image: open("./app/assets/images/default_icon.png"),
-        price: Faker::Number.between(from:1, to:10000),
-        store: Faker::Internet.url,
-        comment: Faker::Hobby.activity,
-        three_main_events: true
-    }
-      ]
+Schedule.all.each do |schedule|
+  5.times {
+    Event.create!(
+      start_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+      end_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+      event_title: Faker::Alphanumeric.alpha(number: 10),
+      image: open("./app/assets/images/default_icon.png"),
+      price: Faker::Number.between(from:1, to:10000),
+      store: Faker::Internet.url,
+      comment: Faker::Hobby.activity,
+      three_main_events: true,
+      schedule: schedule,
     )
   }
 end
