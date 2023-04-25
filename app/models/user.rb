2181e3f0b122a3a036c81e6e82204bc3e3481390
Maekawa Kouki <maekawa.kouki33@gmail.com>
2023-04-25 11:31:58 +0900

    add: already_tried?メソッド作成#134

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,10 @@ class User < ApplicationRecord
     favorites.exists?(schedule_id: schedule.id)
   end
 
+  def already_tried?(schedule)
+    tries.exists?(schedule_id: schedule.id)
+  end
+
   def own?(object)
     id == object.user_id
   end
